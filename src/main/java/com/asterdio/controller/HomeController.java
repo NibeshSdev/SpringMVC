f0910297ba7a263d776acd503c67df1325ae69ac
NibeshSdev <sdev.nibs@gmail.com>
2016-12-02 16:00:57 -0800

    First commit

--- src/main/java/com/asterdio/controller/HomeController.java
+++ src/main/java/com/asterdio/controller/HomeController.java
@@ -3,16 +3,19 @@ package com.asterdio.controller;
 /**
  * Created by Admin on 11/26/2016.
  */
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
 @Controller
 public class HomeController {
     @RequestMapping("/hello")
     public ModelAndView helloWorld() {
-        String message = "HELLO SPRING MVC ";
-        String msg="hello";
+        String message;
+        message = "HELLO SPRING MVC HOW R U";
         return new ModelAndView("hellopage", "message", message);
-
     }
+
 }
+
